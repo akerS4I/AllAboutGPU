31eb9b816b2375f64cf71ea15762c3c4591c8130
AKER <recrentic@gmail.com>
2024-11-16 01:59:13 +0500

    Github Pages set up

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/AllAboutGPU/", // Update this to match your GitHub repository name
 })
